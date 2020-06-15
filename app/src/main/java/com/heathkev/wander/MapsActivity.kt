e5a2221a57aa7b5ee35a872af985e3b702976b5e
heathkev <kevintuazon45@gmail.com>
2020-06-15 21:52:27 +0800

    add markers and camera

--- app/src/main/java/com/heathkev/wander/MapsActivity.kt
+++ app/src/main/java/com/heathkev/wander/MapsActivity.kt
@@ -38,9 +38,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val latitude = 15.223347
+        val longitude = 120.574188
+        val zoomLevel = 15f
+
+        val homeLatLng = LatLng(latitude, longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
